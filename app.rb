e7453b4d6d1a2eb3ed37d91b80c78f7f286132a2
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-18 15:27:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,4 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+
+  get '/' do
+    erb :index
+  end
+  
+  get '/view' do
+    erb :index
+  end
+  
+  post '/puppy' do
+    
+  end
 end
